2fe4e5c78beb4179f1417834f305f4578d0d8ada
TurtleWolfe <JonPohlner@gmail.com>
2020-10-13 14:30:05 -0400

    Ch05 section 8 Creating the Notes application wiring – app.mjs

--- notez/app.mjs
+++ notez/app.mjs
@@ -1,13 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import { default as express } from 'express';
+import { default as hbs } from 'hbs';
+import * as path from 'path';
+// import * as favicon from'serve-favicon';
+import { default as logger } from 'morgan';
+import { default as cookieParser } from 'cookie-parser';
+import { default as bodyParser } from 'body-parser';
+import * as http from 'http';
+import { approotdir } from './approotdir.mjs';
+const __dirname = approotdir;
+import {
+  normalizePort, onError, onListening, handle404, basicErrorHandler
+} from './appsupport.mjs';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import { router as indexRouter } from './routes/index.mjs';
+// import { router as notesRouter }  from './routes/notes.mjs'; 
 
-var app = express();
+export const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,23 +27,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Router function lists
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+// app.use('/notes', notesRouter);
 
+// error handlers
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(handle404);
+app.use(basicErrorHandler);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+export const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+export const server = http.createServer(app);
 
-module.exports = app;
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
