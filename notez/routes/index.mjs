8f3131d1b98b37b1cef78170a9fca3b400ff4390
TurtleWolfe <JonPohlner@gmail.com>
2020-10-13 13:48:11 -0400

    Ch05 section 7 Rewriting the generated router module as an ES6 module

--- notez/routes/index.mjs
+++ notez/routes/index.mjs
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+import { default as express } from 'express';
+export const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  //... placeholder for Notes home page code
+  res.render('index', { title: 'Notes' });
 });
-
-module.exports = router;
