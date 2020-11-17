4cd52a671a64d61fd758448ba171ae200fa5451c
TurtleWolfe <JonPohlner@gmail.com>
2020-11-17 16:00:08 -0500

    Ch 05 section 15

--- notez/routes/notes.mjs
+++ notez/routes/notes.mjs
@@ -38,3 +38,15 @@ router.get('/view', async (req, res, next) => {
         });
     } catch (err) { next(err); }
 });
+
+// Edit note (update)
+router.get('/edit', async (req, res, next) => {
+    try {
+        const note = await notes.read(req.query.key);
+        res.render('noteedit', {
+            title: note ? ("Edit " + note.title) : "Add a Note",
+            docreate: false,
+            notekey: req.query.key, note: note
+        });
+    } catch (err) { next(err); }
+});
